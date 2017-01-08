1c84890abb0efac2c97410c1529ef6a3f9b7a528
arion779 <programmerpro777@gmail.com>
2017-01-08 13:42:01 +0500

    Create createproject.py

---
+++ createproject.py
@@ -0,0 +1,38 @@
+import os
+
+path = "/sdcard/Download/"
+project_name = "Python"
+
+folders = \
+[
+   ["images" , [
+     
+     ["items-opaque", []],
+     ["terrain-atlas", []],
+     ["armor", [] ],
+     ["mob", [] ]
+   
+   ] ],
+   ["script" , [] ]
+
+]
+
+def create_folder(path):
+    if not os.path.exists(path):
+       os.mkdir(path)
+       
+def build(root,data):
+    if data:
+       for d in data:
+           name = d[0]
+           path = os.path.join(root,name)
+           create_folder(path)
+           build(path,d[1])
+           
+       
+full_path = os.path.join(path,project_name) 
+create_folder(full_path)        
+    
+build(full_path,folders)
+print("")
+print (full_path)
